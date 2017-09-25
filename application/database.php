e52748bc190ffc34a630c219d35d2f969f135a72
jack-lijing <lijing48301243@gmail.com>
2017-09-25 09:43:17 +0800

    change database to 172.20.9.60

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '172.20.9.60',
     // 数据库名
     'database'        => 'lpg',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => 'lpg@960',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
