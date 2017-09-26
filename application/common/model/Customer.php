5e3c2f98416ec58fc28bf171a3535a5885642627
mortal <2329867185@qq.comqq.com>
2017-09-26 16:31:24 +0800

    Update Customer.php

--- application/common/model/Customer.php
+++ application/common/model/Customer.php
@@ -7,11 +7,16 @@ class Customer extends Model
 {
 	// 获取所有客户数据
 	public function getAllCustomers(){
-		$order = [
-			'id' => 'asc',
+		$data = [
+			'status' => 0,
 		];
 
-		return $this->order($order)			        
+		$order = [
+			'id' => 'desc',
+		];
+
+		return $this->where($data)
+					->order($order)			        
 			        ->paginate(10);
 	}
 	// 添加用户
@@ -24,10 +29,18 @@ class Customer extends Model
 		return $this->data($data)->allowField(true)
 			->save();
 	}
-
+	// g根据id获取用户信息
 	public function getCustomers($id){
 		$data = [
 			'id' => $id,
+		];
+		return $this->where($data)->find();
+	}
+	
+	// 根据瓶体id获取用户数据
+	public function getUserInfo($bottleId){
+		$data = [
+			'bottleId' => $bottleId,
 		];
 		return $this->where($data)->find();
 	}
